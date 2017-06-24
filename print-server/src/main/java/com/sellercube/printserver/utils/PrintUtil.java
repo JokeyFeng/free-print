0098da15da59cb17afc0b411de25522532a21472
chenjing <1134991001@qq.com>
2017-06-24 14:33:09 +0800

    增加pdf打印

--- print-server/src/main/java/com/sellercube/printserver/utils/PrintUtil.java
+++ print-server/src/main/java/com/sellercube/printserver/utils/PrintUtil.java
@@ -12,7 +12,6 @@ import javax.print.attribute.standard.MediaPrintableArea;
 import javax.print.attribute.standard.OrientationRequested;
 import javax.print.attribute.standard.PrintQuality;
 import java.io.FileInputStream;
-import java.io.IOException;
 
 /**
  * 打印工具类
@@ -26,66 +25,56 @@ public class PrintUtil {
      *
      * @param str 渠道给的一堆文字
      */
-    public static void printByString(String str) {
+    public static void printByString(String str) throws Exception {
         // 构建打印请求属性集
         HashPrintRequestAttributeSet pras = new HashPrintRequestAttributeSet();
         // 设置打印格式，因为未确定类型，所以选择autosense
         DocFlavor flavor = DocFlavor.BYTE_ARRAY.AUTOSENSE;
         // 定位默认的打印服务
         PrintService defaultService = PrintServiceLookup.lookupDefaultPrintService();
-        try {
-            DocPrintJob job = defaultService.createPrintJob(); // 创建打印作业
-            DocAttributeSet das = new HashDocAttributeSet();
-            Doc doc = new SimpleDoc(str.getBytes(), flavor, das);
-            job.print(doc, pras);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        DocPrintJob job = defaultService.createPrintJob(); // 创建打印作业
+        DocAttributeSet das = new HashDocAttributeSet();
+        Doc doc = new SimpleDoc(str.getBytes(), flavor, das);
+        job.print(doc, pras);
     }
 
     /**
      * 打印图片 支持gif jpg png 三种格式
      *
      * @param fileName 文件路径
-     * @param width    宽度
-     * @param height   高度
+     * @param width    宽度 mm
+     * @param height   高度 mm
      */
-    public static void printImage(String fileName, Integer width, Integer height) {
-        try {
-            DocFlavor dof = null;
-            if (fileName.endsWith(".gif")) {
-                dof = DocFlavor.INPUT_STREAM.GIF;
-            } else if (fileName.endsWith(".jpg")) {
-                dof = DocFlavor.INPUT_STREAM.JPEG;
-            } else if (fileName.endsWith(".png")) {
-                dof = DocFlavor.INPUT_STREAM.PNG;
-            }
-            PrintService ps = PrintServiceLookup.lookupDefaultPrintService();
-            PrintRequestAttributeSet pras = new HashPrintRequestAttributeSet();
-            pras.add(OrientationRequested.PORTRAIT);
-            pras.add(new Copies(1));
-            pras.add(PrintQuality.HIGH);
-            DocAttributeSet das = new HashDocAttributeSet();
-            // 设置打印纸张的大小（以毫米为单位）
-            das.add(new MediaPrintableArea(0, 0, width, height, MediaPrintableArea.MM));
-            FileInputStream fin = new FileInputStream(fileName);
-            Doc doc = new SimpleDoc(fin, dof, das);
-            DocPrintJob job = ps.createPrintJob();
-            job.print(doc, pras);
-            fin.close();
-        } catch (IOException ie) {
-            ie.printStackTrace();
-        } catch (PrintException pe) {
-            pe.printStackTrace();
+    public static void printImage(String fileName, Integer width, Integer height) throws Exception {
+        DocFlavor dof = null;
+        if (fileName.endsWith(".gif")) {
+            dof = DocFlavor.INPUT_STREAM.GIF;
+        } else if (fileName.endsWith(".jpg")) {
+            dof = DocFlavor.INPUT_STREAM.JPEG;
+        } else if (fileName.endsWith(".png")) {
+            dof = DocFlavor.INPUT_STREAM.PNG;
         }
+        PrintService ps = PrintServiceLookup.lookupDefaultPrintService();
+        PrintRequestAttributeSet pras = new HashPrintRequestAttributeSet();
+        pras.add(OrientationRequested.PORTRAIT);
+        pras.add(new Copies(1));
+        pras.add(PrintQuality.HIGH);
+        DocAttributeSet das = new HashDocAttributeSet();
+        // 设置打印纸张的大小（以毫米为单位）
+        das.add(new MediaPrintableArea(0, 0, width, height, MediaPrintableArea.MM));
+        FileInputStream fin = new FileInputStream(fileName);
+        Doc doc = new SimpleDoc(fin, dof, das);
+        DocPrintJob job = ps.createPrintJob();
+        job.print(doc, pras);
+        fin.close();
     }
 
     /**
-     * 打印图片 支持gif jpg png 默认宽度高度 10X15
+     * 打印图片 支持gif jpg png 默认宽度高度 100X150
      *
      * @param fileName 文件路径
      */
-    public static void printImage(String fileName) {
+    public static void printImage(String fileName) throws Exception {
         printImage(fileName, 100, 150);
     }
 
@@ -93,47 +82,50 @@ public class PrintUtil {
      * 打印图片
      *
      * @param fileInputStream 图片的输入流
-     * @param width           宽度
-     * @param height          高度
+     * @param width           宽度 mm
+     * @param height          高度 mm
      * @param type            图片的格式
      */
-    public static void printImage(FileInputStream fileInputStream, Integer width, Integer height, String type) {
-        try {
-            DocFlavor dof = null;
-            if (type.endsWith(".gif")) {
-                dof = DocFlavor.INPUT_STREAM.GIF;
-            } else if (type.endsWith(".jpg")) {
-                dof = DocFlavor.INPUT_STREAM.JPEG;
-            } else if (type.endsWith(".png")) {
-                dof = DocFlavor.INPUT_STREAM.PNG;
-            }
-            PrintService ps = PrintServiceLookup.lookupDefaultPrintService();
-            PrintRequestAttributeSet pras = new HashPrintRequestAttributeSet();
-            pras.add(OrientationRequested.PORTRAIT);
-            pras.add(new Copies(1));
-            pras.add(PrintQuality.HIGH);
-            DocAttributeSet das = new HashDocAttributeSet();
-            // 设置打印纸张的大小（以毫米为单位）
-            das.add(new MediaPrintableArea(0, 0, width, height, MediaPrintableArea.MM));
-            FileInputStream fin = fileInputStream;
-            Doc doc = new SimpleDoc(fin, dof, das);
-            DocPrintJob job = ps.createPrintJob();
-            job.print(doc, pras);
-            fin.close();
-        } catch (IOException ie) {
-            ie.printStackTrace();
-        } catch (PrintException pe) {
-            pe.printStackTrace();
+    public static void printImage(FileInputStream fileInputStream, Integer width, Integer height, String type) throws Exception {
+        DocFlavor dof = null;
+        if (type.endsWith(".gif")) {
+            dof = DocFlavor.INPUT_STREAM.GIF;
+        } else if (type.endsWith(".jpg")) {
+            dof = DocFlavor.INPUT_STREAM.JPEG;
+        } else if (type.endsWith(".png")) {
+            dof = DocFlavor.INPUT_STREAM.PNG;
         }
+        PrintService ps = PrintServiceLookup.lookupDefaultPrintService();
+        PrintRequestAttributeSet pras = new HashPrintRequestAttributeSet();
+        pras.add(OrientationRequested.PORTRAIT);
+        pras.add(new Copies(1));
+        pras.add(PrintQuality.HIGH);
+        DocAttributeSet das = new HashDocAttributeSet();
+        // 设置打印纸张的大小（以毫米为单位）
+        das.add(new MediaPrintableArea(0, 0, width, height, MediaPrintableArea.MM));
+        FileInputStream fin = fileInputStream;
+        Doc doc = new SimpleDoc(fin, dof, das);
+        DocPrintJob job = ps.createPrintJob();
+        job.print(doc, pras);
+        fin.close();
     }
 
     /**
-     * 打印图片 默认10X15
+     * 打印图片 默认100X150
      *
      * @param fileInputStream 输入流
      * @param type            图片的格式
      */
-    public static void printImage(FileInputStream fileInputStream, String type) {
+    public static void printImage(FileInputStream fileInputStream, String type) throws Exception {
         printImage(fileInputStream, 100, 150, type);
+    }
+
+    /**
+     * 打印PDF
+     *
+     * @param path pdf路径
+     */
+    public static void printPDF(String path) throws Exception {
+        Runtime.getRuntime().exec("cmd.exe /C start acrord32 /P /h " + path);
     }
 }
