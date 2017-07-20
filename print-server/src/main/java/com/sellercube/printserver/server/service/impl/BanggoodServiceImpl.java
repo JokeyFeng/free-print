f9a00f8be322037ec944b5b117c7e9d278ad27f2
chenjing <1134991001@qq.com>
2017-07-20 17:29:55 +0800

    错误信息完善

--- print-server/src/main/java/com/sellercube/printserver/server/service/impl/BanggoodServiceImpl.java
+++ print-server/src/main/java/com/sellercube/printserver/server/service/impl/BanggoodServiceImpl.java
@@ -3,10 +3,13 @@ package com.sellercube.printserver.server.service.impl;
 import com.alibaba.fastjson.JSONObject;
 import com.sellercube.common.entity.Result;
 import com.sellercube.common.utils.ResultUtil;
+import com.sellercube.printserver.server.entity.Monitor;
+import com.sellercube.printserver.server.mapper.MonitorMapper;
 import com.sellercube.printserver.server.service.BanggoodService;
 import com.sellercube.printserver.utils.CoreUtil;
 import com.sellercube.printserver.utils.PrintUtil;
 import org.apache.commons.codec.binary.Base64;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
@@ -20,20 +23,26 @@ import java.util.Objects;
  */
 @Service
 public class BanggoodServiceImpl implements BanggoodService {
+
+    @Autowired
+    private MonitorMapper monitorMapper;
+
     @Override
     public Result process(JSONObject jsonObject) throws Exception {
+        Integer monitorId = jsonObject.getInteger("monitorId");
         JSONObject data = jsonObject.getJSONObject("Data");
         String pdfUrl = data.getString("PDFUrl");
         String shipType = data.getString("ShipType");
 
         if (Objects.equals(null, pdfUrl) || Objects.equals("", pdfUrl)) {
-            return ResultUtil.error("打印失败，获取不到pdf的url");
+            return ResultUtil.error("打印失败，PDFUrl内容为空");
         }
+        //还原特殊字符串
         pdfUrl = convert(pdfUrl);
         switch (shipType) {
             case "UPSFedex"://Fedex
                 if (pdfUrl.startsWith("iVBORw0KGgo")) {
-                //base64 的图片
+                    //base64 的图片
                     InputStream inputStream = new ByteArrayInputStream(Base64.decodeBase64(pdfUrl));
                     PrintUtil.printImage(inputStream, "png");
                     break;
@@ -48,19 +57,23 @@ public class BanggoodServiceImpl implements BanggoodService {
             case "DPD":
                 //base64 的打印机指令
                 PrintUtil.printByString(new String(Base64.decodeBase64(pdfUrl)));
-                return ResultUtil.error("抱歉，目前DPD渠道fds那边还未更新，所以不能打印");
             default:
-                return ResultUtil.error("打印失败，不支持该渠道");
+                Monitor monitor = new Monitor();
+                monitor.setId(monitorId);
+                monitor.setStatus("打印失败");
+                monitor.setId(monitorId);
+                monitorMapper.updateByPrimaryKeySelective(monitor);
+                return ResultUtil.error("打印失败，不支持" + shipType + "渠道");
         }
-
+        monitorMapper.deleteByPrimaryKey(monitorId);
         return ResultUtil.success("打印成功");
     }
 
     /**
-     * .net那边对字符串进行了转换，现在转换过来
+     * .net那边对特殊字符进行了转换，现在转换过来
      *
      * @param str 字符串
-     * @return 原生的base64
+     * @return 原生的数据
      */
     private static String convert(String str) {
         return StringUtils.replace(str, "--jiahao--", "+")
--- print-server/src/main/java/com/sellercube/printserver/server/service/impl/FlytServiceImpl.java
+++ print-server/src/main/java/com/sellercube/printserver/server/service/impl/FlytServiceImpl.java
@@ -3,23 +3,31 @@ package com.sellercube.printserver.server.service.impl;
 import com.alibaba.fastjson.JSONObject;
 import com.sellercube.common.entity.Result;
 import com.sellercube.common.utils.ResultUtil;
+import com.sellercube.printserver.server.entity.Monitor;
+import com.sellercube.printserver.server.mapper.MonitorMapper;
 import com.sellercube.printserver.server.service.FlytService;
 import com.sellercube.printserver.utils.CoreUtil;
 import com.sellercube.printserver.utils.PrintUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Objects;
 
 /**
- *飞特接口
+ * 飞特接口
  * Created by Chenjing on 2017/7/15.
  */
 @Service
 public class FlytServiceImpl implements FlytService {
+
+    @Autowired
+    private MonitorMapper monitorMapper;
+
     @Override
     public Result process(JSONObject jsonObject) throws Exception {
         String path = null;
         JSONObject data = jsonObject.getJSONObject("Data");
+        Integer monitorId = jsonObject.getInteger("monitorId");
         switch (data.getString("Format")) {
             case "PDF":
                 path = CoreUtil.base64(data.getString("Label"), "pdf");
@@ -33,9 +41,14 @@ public class FlytServiceImpl implements FlytService {
 
         if (!Objects.equals(null, path) && !Objects.equals("", path)) {
             PrintUtil.printPDF(path);
+            monitorMapper.deleteByPrimaryKey(monitorId);
             return ResultUtil.success("打印成功");
         }
-
+        Monitor monitor = new Monitor();
+        monitor.setId(monitorId);
+        monitor.setStatus("打印失败");
+        monitor.setId(monitorId);
+        monitorMapper.updateByPrimaryKeySelective(monitor);
         return ResultUtil.error("打印失败");
     }
 }
