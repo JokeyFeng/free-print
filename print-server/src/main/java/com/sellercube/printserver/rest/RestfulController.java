ffd2f2faf53c329657052d84f0b4bf3e525c3424
JokeyFeng <976024396@qq.com>
2018-05-17 10:54:46 +0800

    Update RestfulController.java

--- print-server/src/main/java/com/sellercube/printserver/rest/RestfulController.java
+++ print-server/src/main/java/com/sellercube/printserver/rest/RestfulController.java
@@ -9,6 +9,7 @@ import com.sellercube.printserver.entity.Printer;
 import com.sellercube.printserver.service.BanggoodService;
 import com.sellercube.printserver.service.PrintService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
